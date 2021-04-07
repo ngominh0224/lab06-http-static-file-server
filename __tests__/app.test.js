b43672867b76e6e24c1cc286ded2c294cd35a9e8
Minh Ngo <ngo.minh0224@gmail.com>
2021-04-07 08:54:15 -0700

    tests pass and destructured

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,14 +2,16 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 describe('app routes', () => {
-  it('gets the / route and returns hi', async () => {
-    const body = await request(app).get('/index.html');
+  it('gets the /index.html route and returns hi', async() => {
+    const body = await request(app)
+      .get('/index.html');
 
-    expect(body.text).toEqual('hi');
+    expect(body.text).toEqual('<h1>hi</h1>');
   });
 
-  it('returns error 404', async () => {
-    const body = await request(app).get('/poo');
+  it('returns error 404', async() => {
+    const body = await request(app)
+      .get('/poo');
 
     expect(body.text).toEqual('Not Found');
   });
